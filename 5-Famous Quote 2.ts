f2e0db6bf88047567f92a6f63895db7afb535199
AlishbaAslam <162062529+AlishbaAslam@users.noreply.github.com>
2024-03-04 03:17:09 +0500

    Add files via upload

---
+++ 5-Famous Quote 2.ts
@@ -0,0 +1,4 @@
+// Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+let famous_person : string ="Imran Khan";
+let message : string = (`${famous_person} once said,“You Only Lose When You Give Up,If You Don't Give Up You Will Win From The Most Incredible Position”`);
+console.log(message);
